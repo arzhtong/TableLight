acffaeb690816ae0945a43b21b482c6331606298
arzhtong@outlook.com <arzhtong@outlook.com>
2021-08-24 01:31:05 +0100

    Add scenario for turning on table light

---
+++ TableLight/src/main/java/uk/TableLight/App.java
@@ -0,0 +1,16 @@
+package uk.TableLight;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+    }
+}
